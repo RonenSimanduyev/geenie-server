57ecad2d4cbd6d085c4413cb5674ff8fcd4b8a54
Ronen <ronen.simanduyev1999@gmail.com>
2023-03-08 11:41:43 +0200

    added a featur to the scrapper

--- server/scrapper.py
+++ server/scrapper.py
@@ -5,7 +5,6 @@ import pandas as pd
 import re
 import math
 from time import time
-from time import sleep
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
