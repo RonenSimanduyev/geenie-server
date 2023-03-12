733fc44ef1d37903909afe44c523a0f3a88050a7
Ronen <ronen.simanduyev1999@gmail.com>
2023-03-12 10:15:26 +0200

    removed client

--- client/src/server/api/routers/info/gpt.ts
+++
@@ -1,7 +0,0 @@
-import { ChatGPTUnofficialProxyAPI } from "chatgpt";
-import { env } from "~/env.mjs";
-
-export const gptClient = new ChatGPTUnofficialProxyAPI({
-  accessToken: env.OPENAI_ACCESS_TOKEN,
-  apiReverseProxyUrl: "https://chat.duti.tech/api/conversation",
-});
